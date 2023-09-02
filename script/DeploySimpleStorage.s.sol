2e259d232536ba37719777339468515e705867c8
hhhfriends19 <84479916@qq.com>
2023-09-02 14:26:55 +0800

    update DeploySimpleStorage.s.sol

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -3,6 +3,10 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Script.sol";
-import "../src/SimpleStorage.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
 
-contract DeploySimpleStorage is Script {}
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcask();
+    }
+}
